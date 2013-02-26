73926ece4dad4a9f32e6fe557081d46e57af47a1
Magnus Skog <magnus.m.skog@gmail.com>
2013-02-26 19:39:50 +0100

    Spec cleanup

--- spec/lib/stew/store/sales_spec.rb
+++ spec/lib/stew/store/sales_spec.rb
@@ -5,10 +5,4 @@ require 'spec_helper'
 describe Stew::Store::Sales do
   let(:response){File.new("spec/fixtures/store/sales/sales.txt").read}
   subject{Stew::Store::Sales.new(response)}
-
-  describe "sales" do
-    it "has the expected sales" do
-      subject.entries.first.name.should eq "Call of Duty®: Black Ops II"
-    end
-  end
 end
